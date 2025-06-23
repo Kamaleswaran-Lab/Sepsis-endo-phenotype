f31441cc98a751ac7fabaccfe31c4e25d0ef37f6
Min Huang <64031030+warming151@users.noreply.github.com>
2025-06-23 16:07:10 -0400

    update

--- code/seurat_integration.R
+++ code/seurat_integration.R
@@ -18,27 +18,32 @@ merged_obj <- NormalizeData(merged_obj)
 merged_obj <- FindVariableFeatures(merged_obj)
 merged_obj <- ScaleData(merged_obj)
 merged_obj <- RunPCA(merged_obj)
-merged_obj <- IntegrateLayers(object = merged_obj, method = RPCAIntegration, orig.reduction = "pca", new.reduction = "integrated.rpca",
+merged_obj <- IntegrateLayers(object = merged_obj, method = HarmonyIntegration, orig.reduction = "pca", new.reduction = "harmony",
                               verbose = FALSE)
+# alldata <- IntegrateLayers(
+#   object = alldata, method = HarmonyIntegration,
+#   orig.reduction = "pca", new.reduction = "harmony",
+#   verbose = FALSE
+# )
 merged_obj[["RNA"]] <- JoinLayers(merged_obj)
 
-merged_obj <- FindNeighbors(merged_obj, reduction = "integrated.rpca", dims = 1:30)
+merged_obj <- FindNeighbors(merged_obj, reduction = "harmony", dims = 1:30)
 merged_obj <- FindClusters(merged_obj, resolution = 1)
-merged_obj <- RunUMAP(merged_obj, dims = 1:30, reduction = "integrated.rpca")
+merged_obj <- RunUMAP(merged_obj, dims = 1:30, reduction = "harmony")
 
-DimPlot(merged_obj, reduction = "integrated.rpca")
+DimPlot(merged_obj, reduction = "harmony")
 
-marker = c("CD79A","CD79B","XBP1",#b plasma
-            "FCGR3A","CD14","S100A9",#CD14 CD16 Mono
-            "CD3D","CD3G", "CCR7","LEF1","TCF7","IL7R","LTB",#cd4
-            "CD8A","CCL5","GZMH","TRGC2",#cd8
-            "FCER1A","CST3","JCHAIN","MZB1",#DC
-            "HBB",#Erythro
-            "HLA-DQA1",#M1 Macro
-            "S100A8","MMP8","MMP9","NAMPT","NEAT1","LCN2","PADI4","ITGAM",#Neutrophil
-            "MPO","ELANE","AZU1",#Neutrophil_progenitors
-            "GNLY","NKG7","PRF1","FGFBP2","GZMA",#nk
-            "ITGA2B","ITGB3","GP1BA","SELP"#Platelets
+marker = c("CD79A","CD79B","XBP1",
+            "FCGR3A","CD14","S100A9",
+            "CD3D","CD3G", "CCR7","LEF1","TCF7","IL7R","LTB",
+            "CD8A","CCL5","GZMH","TRGC2",
+            "FCER1A","CST3","JCHAIN","MZB1",
+            "HBB",
+            "HLA-DQA1",
+            "S100A8","MMP8","MMP9","NAMPT","NEAT1","LCN2","PADI4","ITGAM",
+            "MPO","ELANE","AZU1",
+            "GNLY","NKG7","PRF1","FGFBP2","GZMA",
+            "ITGA2B","ITGB3","GP1BA","SELP"
 )
 
 
